68c9dcde5133b6162fd0afc925c7260209da716c
Florn67 <edgewalker909@gmail.com>
2021-07-17 23:05:02 +0300

    pre_second

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,26 @@
 let closeButton = document.querySelector('.popup__close-button');
 let editButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
-let submitButton = document.querySelector('.popup__submit');
 let form = document.querySelector('.popup__container');
-let nameInput = document.querySelector('.popup__name');
-let descriptionInput = document.querySelector('.popup__description');
+let nameInput = document.querySelector('.popup__input_value_name');
+let descriptionInput = document.querySelector('.popup__input_value_description');
 let profileName = document.querySelector('.profile__name');
 let profileDescription = document.querySelector('.profile__description');
 
 function openPopup(){
+    nameInput.value = profileName.textContent;
+    descriptionInput.value = profileDescription.textContent;
     popup.classList.add('popup_opened');
-    console.log('11');
 }
 
 function closePopup(){
-    nameInput.value = profileName.textContent;
-    descriptionInput.value = profileDescription.textContent;
     popup.classList.remove('popup_opened');
-    console.log('11');
 }
 
 function changeInfo(evt){
     evt.preventDefault();
-    nameInpitValue = nameInput.value;
-    descriptionInputValue = descriptionInput.value;
-    profileName.textContent = nameInpitValue;
-    profileDescription.textContent = descriptionInputValue;
+    profileName.textContent = nameInput.value;
+    profileDescription.textContent = descriptionInput.value;
     closePopup();
 }
 
