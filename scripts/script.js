a19aa54dde76798c5c650f3ed2c1433341f3caf9
Florn67 <edgewalker909@gmail.com>
2021-09-19 18:37:06 +0300

    prewebpack

--- scripts/script.js
+++ scripts/script.js
@@ -2,29 +2,20 @@ import Card from './Card.js'
 import FormValidator from './FormValidator.js';
 import {openPopup, closePopup} from './utils/utils.js'
 
-import Popup from '../../components/Popup.js'
 import PopupWithImage from '../../components/PopupWithImage.js'
 import Section from '../../components/Section.js'
 import UserInfo from '../../components/UserInfo.js'
 import PopupWithForm from '../components/PopupWithForm.js';
 
-const popups = Array.from(document.querySelectorAll('.popup'));
 const editButton = document.querySelector('.profile__edit-button');
 const addButton = document.querySelector('.profile__add-button');
-const popupEdit = document.querySelector('.popup_type_edit');
-const popupAdd = document.querySelector('.popup_type_add');
 const formEdit = document.querySelector('.popup__form_type_edit');
 const formAdd = document.querySelector('.popup__form_type_add');
 const nameInputEdit = document.querySelector('.popup__input_value_name-edit');
 const nameInputAdd = document.querySelector('.popup__input_value_name-add');
 const descriptionInputEdit = document.querySelector('.popup__input_value_description-edit');
 const descriptionInputAdd = document.querySelector('.popup__input_value_description-add');
-const profileName = document.querySelector('.profile__name');
-const profileDescription = document.querySelector('.profile__description');
-
-const cards = document.querySelector('.elements');
 const cardTemplate = '#card-template';
-const submitButtonAdd = document.querySelector('.popup__submit_type_add');
 
 
 const validationOptions = {
@@ -74,22 +65,9 @@ editButton.addEventListener('click', function() {
     nameInputEdit.value = profileInfo.name;
     descriptionInputEdit.value = profileInfo.info;
     editProfileFormValidator.hideAllErrors();
-    openPopup(popupEdit);
+    popupTypeEdit.open();
 });
 addButton.addEventListener('click', function(){ 
     addCardFormValidator.hideAllErrors();
-    openPopup(popupAdd)
+    popupTypeAdd.open('add');
 });
-
-// formAdd.addEventListener('submit', function(evt){
-//     evt.preventDefault();
-//     const newCard = new Section({items: [{name: nameInputAdd.value, link: descriptionInputAdd.value}], renderer: (item) => {
-//         const cardElement = new Card(item, cardTemplate).generateCard()
-//         newCard.setItem(cardElement, 'prepend')
-//     }},'.elements')
-//     newCard.renderItems();
-//     nameInputAdd.value = "";
-//     descriptionInputAdd.value ="";
-//     addCardFormValidator.disableSubmitButton()
-//     closePopup(popupAdd);
-// });
